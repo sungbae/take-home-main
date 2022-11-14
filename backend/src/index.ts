9e35f6e0c81c7d7d7eea34eaf222c78e7ee0ce3a
sungbae <sungbae305@gmail.com>
2022-11-13 21:40:12 -0700

    set startup in backend instead of frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import express from "express"
 import http from "http"
 import { createAndConnectToServer } from "./db"
 import { recipeMiddleware, searchMiddleware } from "./routes"
+const path = require("path")
 
 const PORT = process.env.PORT || 4000
 
@@ -12,10 +13,18 @@ const appStartup = async (): Promise<void> => {
   // add parsers for the body
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: false }))
+
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")))
+
   // create our routes
   app.get("/api/recipe/:id", recipeMiddleware)
   app.post("/api/search", searchMiddleware)
   // create a server
+
+  app.get("*", function (request, response) {
+    // response.sendFile(buildTemplate)
+    response.sendFile(__dirname + "/../../frontend/dist/index-template.html")
+  })
 
   const httpServer = new http.Server(app)
   httpServer.listen(+PORT, "0.0.0.0", () => {

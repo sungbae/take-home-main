812174eef54e82cd532afc41469ad70ba7ffa92d
sungbae <sungbae305@gmail.com>
2022-11-13 17:36:47 -0700

    see if prd deploy to heroku works

---
+++ frontend/server.js
@@ -0,0 +1,21 @@
+const path = require("path")
+const express = require("express")
+const app = express()
+const PORT = process.env.PORT
+
+const staticDir = path.resolve(__dirname, "static")
+const buildTemplate = path.resolve(staticDir, "index-template.html")
+
+app.use(express.static(staticDir))
+
+app.get("/", function (request, response) {
+  response.sendFile(buildTemplate)
+})
+
+app.listen(PORT, (error) =>
+  error
+    ? console.error(error)
+    : console.info(
+        `Listening on port ${PORT}. Visit http://localhost:${PORT}/ in your browser.`
+      )
+)

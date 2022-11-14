60d92afe820a4f284cfdb5a75e2921d3bf0a1909
sungbae <sungbae305@gmail.com>
2022-11-13 22:01:08 -0700

    use multi procfiles again

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,6 @@ import express from "express"
 import http from "http"
 import { createAndConnectToServer } from "./db"
 import { recipeMiddleware, searchMiddleware } from "./routes"
-const path = require("path")
 
 const PORT = process.env.PORT || 4000
 
@@ -14,18 +13,10 @@ const appStartup = async (): Promise<void> => {
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({ extended: false }))
 
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")))
-
   // create our routes
   app.get("/api/recipe/:id", recipeMiddleware)
   app.post("/api/search", searchMiddleware)
   // create a server
-
-  app.get("*", function (request, response) {
-    // response.sendFile(buildTemplate)
-    response.sendFile(__dirname + "/../../frontend/dist/index-template.html")
-  })
-
   const httpServer = new http.Server(app)
   httpServer.listen(+PORT, "0.0.0.0", () => {
     console.log("now running on " + PORT)

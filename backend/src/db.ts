8bea430e5a0bda0265e9b13c72c1f61f2d52f311
sungbae <sungbae305@gmail.com>
2022-11-13 23:05:28 -0700

    add debug lines to db.ts

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,5 +1,5 @@
-import mongoose from "mongoose"
 import { MongoMemoryServer } from "mongodb-memory-server"
+import mongoose from "mongoose"
 import { builtRecipes } from "./data"
 import { RecipeModel } from "./models"
 
@@ -15,13 +15,17 @@ export const createAndConnectToServer = async (): Promise<typeof mongoose> => {
   const mongod = new MongoMemoryServer({
     autoStart: false,
   })
+  console.log("db line 18")
   await mongod.start()
+  console.log("db line 20")
   const url = await mongod.getConnectionString()
+  console.log("db line 22")
   const connection = await mongoose.connect(url, {
     useNewUrlParser: true,
     keepAlive: true,
     connectTimeoutMS: 30000,
   })
+  console.log("db line 28")
   // add default recipes
   await createRecipes()
   return connection
